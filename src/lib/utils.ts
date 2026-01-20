96f4384bc564c5222ebc763a3857a525a2aea088
Mark Janveaux <mjanveaux@users.noreply.github.com>
2026-01-20 00:34:00 -0700

    feat: CRUD implementation for collections. Couple of unique design decisions to note - the use of a discriminated union within a Generic 'ActionResult' (see src/lib/types.ts) as well as the use of inferred types using z.infer.

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,15 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
+import { ActionResult } from "./types"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function errorResponse(message: string, errors?: Record<string, string[]>): ActionResult<any> {
+  return { success: false, message, errors };
+}
+
+export function successResponse<T>(data: T, message?: string): ActionResult<T> {
+  return { success: true, data, message };
 }
